9403ab39dfcd8baca53fe0addff59c68601d3b42
IanDoarn <Ian.Doarn@zimmerbiomet.com>
2017-01-09 20:33:01 -0600

    First Commit

---
+++ bot.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from dart import handles
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Build program latest version')
+    parser.add_argument('-r', '--run', nargs=1, help='Run a new bot', required=False, metavar='[TOKEN]')
+    parser.add_argument('-j', '--join-test', action='store_true', help='Join Test Server', required=False)
+
+    args = vars(parser.parse_args())
+
+    if args['run']:
+        handles.main(args['run'][0])
+
+    if args['join_test']:
+        handles.main('MjY3NTA1MDY2MzA5NzEzOTMw.C1QcmA.OVS6j0lFRkGEEyccz7G0rIygrEA')
+
+
