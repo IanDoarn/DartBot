5831b72e1fb7810e34aa5c8aec676b43d2bc5c75
theDartrunner <thedartrunner@gmail.com>
2017-01-25 19:05:25 -0800

    Bot now has music functionality: volume control, pause/resume, skip, np,

--- dart/handles.py
+++ dart/handles.py
@@ -12,7 +12,7 @@ class VoiceEntry:
         fmt = '*{0.title}* uploaded by {0.uploader} and requested by {1.display_name}'
         duration = self.player.duration
         if duration:
-            fmt = fmt + ' [length: {0[0]}m {0[1]}s]'.format(divmod(duration, 60))
+            fmt += ' [length: {0[0]}m {0[1]}s]'.format(divmod(duration, 60))
         return fmt.format(self.player, self.requester)
 
 
@@ -23,7 +23,7 @@ class VoiceState:
         self.bot = client_
         self.play_next_song = asyncio.Event()
         self.songs = asyncio.Queue()
-        self.skip_votes = set() # a set of user_ids that voted
+        self.skip_votes = set()  # a set of user_ids that voted
         self.audio_player = self.bot.loop.create_task(self.audio_player_task())
 
     def is_playing(self):
@@ -46,12 +46,12 @@ class VoiceState:
         self.bot.loop.call_soon_threadsafe(self.play_next_song.set)
 
     async def audio_player_task(self):
-        print('audio_player_task')
+        """print('audio_player_task')"""
         while True:
-            print('true')
+            '''print('true')'''
             self.play_next_song.clear()
             self.current = await self.songs.get()
-            print(self.current.channel)
+            '''print(self.current.channel)'''
             await self.bot.send_message(self.current.channel, 'Now playing ' + str(self.current))
             self.current.player.start()
             await self.play_next_song.wait()
@@ -73,11 +73,6 @@ class Music:
 
         return state
 
-    async def create_voice_client(self, channel):
-        voice = await self.bot.join_voice_channel(channel)
-        state = self.get_voice_state(channel.server)
-        state.voice = voice
-
     def __unload(self):
         for state in self.voice_states.values():
             try:
@@ -89,20 +84,20 @@ class Music:
 
 
 def pm(destination, message):
-    #function used to send pms to users
+    """function used to send pms to users"""
     return
 
 async def join_channel(message):
     server = message.server
     bot = server.me
     # print(server.channels)
-    #print(message.content)
+    # print(message.content)
     msg = 'joinVoice'
     if len(message.content) > 10:
-        channelName = message.content[11:]
+        channel_name = message.content[11:]
         for channels in server.channels:
             perms = channels.permissions_for(bot)
-            if channels.name == channelName:
+            if channels.name == channel_name:
                 if channels.type.name == 'voice':
                     if str(perms.value)[2] == '4':
                         voice = await client.join_voice_channel(channels)
@@ -120,7 +115,7 @@ async def join_channel(message):
 
     else:
         for channels in server.channels:
-            #print(channels)
+            # print(channels)
             perms = channels.permissions_for(bot)
             if channels.type.name == 'voice' and str(perms.value)[2] == '4':
                 voice = await client.join_voice_channel(channels)
@@ -135,13 +130,13 @@ async def change_channel(message):
     voice = server.voice_client
     bot = server.me
     # print(server.channels)
-    #print(message.content)
+    # print(message.content)
     msg = 'joinVoice'
     if len(message.content) > 10:
-        channelName = message.content[11:]
+        channel_name = message.content[11:]
         for channels in server.channels:
             perms = channels.permissions_for(bot)
-            if channels.name == channelName:
+            if channels.name == channel_name:
                 if channels.type.name == 'voice':
                     if str(perms.value)[2] == '4':
                         await voice.move_to(channels)
@@ -157,7 +152,7 @@ async def change_channel(message):
 
     else:
         for channels in server.channels:
-            #print(channels)
+            # print(channels)
             perms = channels.permissions_for(bot)
             if channels.type.name == 'voice' and str(perms.value)[2] == '4':
                 await voice.move_to(channels)
@@ -167,7 +162,7 @@ async def change_channel(message):
 
 client = discord.Client()
 music = Music(client)
-
+command = '!'  # just have to change this line to change command prefix
 
 
 @client.event
@@ -177,57 +172,57 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+
 @client.event
 async def on_message(message):
-    #print(message.content)
+    # print(message.content)
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        #print(message.content)
+    if message.content.startswith(command + 'hello'):
+        # print(message.content)
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('!help'):
-        #print(message.content)
+    if message.content.startswith(command + 'help'):
+        # print(message.content)
         msg = 'I need an adult!'
         await client.send_message(message.channel, msg)
 
     if message.content.startswith('tot'):
-        #print(message.content)
+        # print(message.content)
         msg = 'gay'
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('!joinVoice'):
+    if message.content.startswith(command + 'joinVoice'):
         server = message.server
         try:
             if server.voice_client.is_connected:
-                #print('changing channel')
+                # print('changing channel')
                 msg = await change_channel(message)
         except:
-            #print('joining new channel')
+            # print('joining new channel')
             msg = await join_channel(message)
 
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('!leave'):
+    if message.content.startswith(command + 'leave'):
         server = message.server
         voice = client.voice_client_in(server)
-        #print(voice.channel.name)
+        # print(voice.channel.name)
         await voice.disconnect()
-        #print(client.is_voice_connected(server))
+        # print(client.is_voice_connected(server))
         msg = 'Disconnected from ' + voice.channel.name
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('!bro'):
+    if message.content.startswith(command + 'bro'):
         msg = "what the fuck"
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('!add'):
+    if message.content.startswith(command + 'add'):
         server = message.server
-        state = Music.get_voice_state(music, server)
-        msg = 'add song'
+        state = music.get_voice_state(music, server)
         if client.is_voice_connected(server):
             voice = client.voice_client_in(server)
             if len(message.content) <= 5:
@@ -238,28 +233,74 @@ async def on_message(message):
                 msg = 'Enqueued ' + str(entry)
                 await state.songs.put(entry)
                 player.volume = 0.6
-                #player.start()
+                # player.start()
         else:
             msg = "I am not in a voice channel, please add me to one first."
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('!disconnect'):
+    if message.content.startswith(command + 'disconnect'):
         if message.author.id == '144634215693156353':
-            #print(message.content)
+            # print(message.content)
             await client.close()
         else:
             await client.send_message(message.channel, 'You do not have permission to use that command')
 
-    '''if message.content.startswith('!test'):
-        server = message.server
-        msg = 'testing'
-        print('perms testing')
-        print(' ')
-        bot = server.me
-        for channels in server.channels:
-            print(channels)
-            perms = channels.permissions_for(bot)
-            print(str(perms.value)[2])'''
+    if message.content.startswith(command + 'volume'):
+        state = music.get_voice_state(message.server)
+        msg = 'volume'
+        if state.is_playing():
+            if len(message.content) <= 7:
+                msg = "Please include a number between 0 and 100"
+            else:
+                if state.is_playing():
+                    player = state.player
+                    player.volume = message.content[8:] / 100
+                    msg = ('Set the volume to {:.0%}'.format(player.volume))
+        else:
+            msg = 'No song currently playing.'
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith(command + 'skip'):
+        state = music.get_voice_state(message.server)
+        if not state.is_playing():
+            msg = 'Not playing any music right now.'
+
+        voter = message.author
+        if voter == state.current.requester:
+            msg = 'Requester requested skipping song.'
+            state.skip()
+        elif voter.id not in state.skip_votes:
+            state.skip_votes.add(voter.id)
+            total_votes = len(state.skip_votes)
+            if total_votes >= 3:
+                msg = 'Skip vote passed, skipping song.'
+                state.skip()
+            else:
+                msg = 'Skip vote added, currently at [{}/3]'.format(total_votes)
+        else:
+            msg = 'You have already voted to skip this song.'
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith(command + 'playing'):
+        state = music.get_voice_state(message.server)
+        if state.current is None:
+            msg = 'Not playing anything.'
+        else:
+            skip_count = len(state.skip_votes)
+            msg = 'Now playing {} [skips: {}/3]'.format(state.current, skip_count)
+        await client.send_message(message.channel, msg)
+
+    if message.content.startswith(command + 'pause'):
+        state = music.get_voice_state(message.server)
+        if state.is_playing():
+            player = state.player
+            player.pause()
+
+    if message.content.startswith(command + 'resume'):
+        state = music.get_voice_state(message.server)
+        if state.is_playing():
+            player = state.player
+            player.resume()
 
 
 def main(token):
