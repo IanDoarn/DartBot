3061779beef4b7f4ac0f6d71d98e4d9f3c6aeabe
theDartrunner <thedartrunner@gmail.com>
2017-10-15 14:38:26 -0700

    Merge branch 'master' of D:\Python Programs\DartBot with conflicts.

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 
 from dart import handles
 
-token = 'MjY3NTA1MDY2MzA5NzEzOTMw.C1QcmA.OVS6j0lFRkGEEyccz7G0rIygrEA' # discord bot app token
+token = "MjY3NTA1MDY2MzA5NzEzOTMw.DMRo8g.fRu3P9JgNiwEK-THtGr88pSYU4Y"# discord bot app token
 
 if __name__ == '__main__':
     import argparse
